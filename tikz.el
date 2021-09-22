1d094d7443a56dc73429c0177a814fe4161b7d84
Jens Schneider <jens.schneider.ac@gmail.com>
2021-09-22 11:59:04 +0200

    Introduce version 0.13

--- tikz.el
+++ tikz.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020  Emilio Torres-Manzanera <torres@uniovi.es>
 
 ;; Author: Emilio Torres-Manzanera <torres@uniovi.es>
-;; Version: 0.12
+;; Version: 0.13
 ;; Keywords: tex
 ;; URL: https://github.com/emiliotorres/tikz
 ;; Package-Requires: ((emacs "24.1"))
